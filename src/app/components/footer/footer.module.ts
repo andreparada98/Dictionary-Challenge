1adee676383c66565f555011f0e5a42ad6e68686
Andre Parada <andreparada98@gmail.com>
2022-10-26 04:14:40 -0300

    add footer component

---
+++ src/app/components/footer/footer.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer.component';
+import { ButtonModule } from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {CardModule} from 'primeng/card';
+
+
+@NgModule({
+  declarations: [FooterComponent],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    InputTextModule,
+    InputTextareaModule,
+    CardModule
+  ],
+  exports:[
+    FooterComponent
+  ]
+})
+export class FooterModule { }
